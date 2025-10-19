fcef44a49a186a287024f70f5c804429b809aeed
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-10-20 00:00:36 +0300

    add custom AppBar with logo and search icon.

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image(
+          image: AssetImage(AssetsData.logo),
+          height: 20,
+        ),
+        Icon(Icons.search),
+      ],
+    );
+  }
+}
