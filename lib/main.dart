d1726a50c6b1fc6e0153b9d3d708d3652e43ec4d
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-10-20 22:14:39 +0300

    add Google Fonts package and apply text theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -13,8 +14,10 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme:
-          ThemeData.dark().copyWith(scaffoldBackgroundColor: Color(0xff100b20)),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xff100b20),
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       home: SplashView(),
     );
   }
