e93c9c7d4ab8fa687009459f3614675cb56b9e49
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-11-12 17:38:43 +0200

    add featured books item to home details view.

--- lib/features/home/presentation/views/widgets/home_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/home_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +7,15 @@ class HomeDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Column(
       children: [
         HomeDetailsAppBar(),
         SizedBox(height: 24),
+        SizedBox(
+          height: height * 0.35,
+          child: FeaturedBooksItem(),
+        ),
       ],
     );
   }
