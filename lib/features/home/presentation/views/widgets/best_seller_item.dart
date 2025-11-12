439a219cafc70bd40517506a2aa20a2471aa0341
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-11-12 16:10:15 +0200

    Navigate to home details view.

--- lib/features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,54 +1,61 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/text_styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_item.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerItem extends StatelessWidget {
   const BestSellerItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          FeaturedBooksItem(),
-          SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.53,
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    style: StyleOfText.textStyle20,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'J.K. Rowling',
-                  style: StyleOfText.textStyle14,
-                ),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '19.99 €',
-                      style: StyleOfText.textStyle20
-                          .copyWith(fontWeight: FontWeight.w600),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kHomeDetailsView);
+      },
+      child: SizedBox(
+        height: 140,
+        child: Row(
+          children: [
+            FeaturedBooksItem(),
+            SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.53,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style: StyleOfText.textStyle20,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    BookRating(),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'J.K. Rowling',
+                    style: StyleOfText.textStyle14,
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '19.99 €',
+                        style: StyleOfText.textStyle20
+                            .copyWith(fontWeight: FontWeight.w600),
+                      ),
+                      BookRating(),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
