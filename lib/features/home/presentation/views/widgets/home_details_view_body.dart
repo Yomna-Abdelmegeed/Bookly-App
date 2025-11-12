423219adacb6c1ccc2a475a8d826990ed9712c66
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-11-12 17:56:49 +0200

    add texts to home details view.

--- lib/features/home/presentation/views/widgets/home_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/text_styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/home_details_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ class HomeDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Column(
       children: [
         HomeDetailsAppBar(),
@@ -16,6 +19,23 @@ class HomeDetailsViewBody extends StatelessWidget {
           height: height * 0.35,
           child: FeaturedBooksItem(),
         ),
+        SizedBox(height: 42),
+        SizedBox(
+          width: width * 0.86,
+          child: Text(
+            'The Jungle Book',
+            style: StyleOfText.textStyle30,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: 13),
+        Text(
+          'Rudyard Kipling',
+          style: StyleOfText.textStyle18
+              .copyWith(color: Colors.white.withValues(alpha: 0.7)),
+        ),
+        SizedBox(height: 20),
+        BookRating(),
       ],
     );
   }
