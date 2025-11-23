6a705c7fab382755b5d6327753f950292c94d43b
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-11-23 21:56:06 +0200

    create  Newest books method.

--- lib/features/home/presentation/views_model/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/views_model/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/item.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
-class NewestBooksCubitCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubitCubit() : super(NewestBooksInitial());
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+
+  
+  final HomeRepo homeRepo;
+
+  Future<void> getFeaturedBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(errMessage: failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
 }
