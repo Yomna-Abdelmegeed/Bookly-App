b9f48d87c1eec582b4c69efdac281c090e64d64d
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-11-24 01:38:00 +0200

    use Cached Network Image.

--- lib/features/home/presentation/views/widgets/featured_books_item.dart
+++ lib/features/home/presentation/views/widgets/featured_books_item.dart
@@ -1,17 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksItem extends StatelessWidget {
-  const FeaturedBooksItem({super.key, required this.imageURL});
+  const FeaturedBooksItem({super.key, required this.imageURL });
   final String imageURL;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.3 / 2,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image:
-              DecorationImage(image: NetworkImage(imageURL), fit: BoxFit.fill),
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      child: AspectRatio(
+        aspectRatio: 1.3 / 2,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageURL,
+          placeholder: (context, url) =>
+              Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) => Icon(Icons.error),
         ),
       ),
     );
